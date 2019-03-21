b3e550f1313ab067a76067e91e7615974500876b
Harsh Dangi <dangiharsh@gmail.com>
2019-03-21 18:08:27 +0530

    Added feature - 'suggest alternate if not found'

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -7,7 +8,17 @@ def translate(word):
     if word in data:
         return data[word]
     else:
-        print("Word not found")
+        match = get_close_matches(word,data.keys())
+        if len(match)>0:
+            choice = input('Did you mean "%s" instead ? '%match[0]).lower()
+            if choice == "y":
+                return data[match[0]]
+            elif choice == "n":
+                return "XXX Word not found XXX"
+            else:
+                return "We didn't understand your query"
+        else:
+            return "XXX Word not found XXX"
 
 word = input("Enter the word: ")
 
